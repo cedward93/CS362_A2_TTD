6f4c83981926841dcfb6cf5c8b884e9b62297201
cedward93 <cedward1993@gmail.com>
2025-05-12 22:16:58 -0700

    added code to verify that a password has an uppercase letter

--- check_pwd.py
+++ check_pwd.py
@@ -4,10 +4,21 @@ def check_pwd(pwd: str) -> bool:
         return False
     if len(pwd) > 20:
         return False
+
+    # creating variables to test if elements are present in the string
     has_lower = False
+    has_upper = False
+
+    # itterating through the string to search for elements
     for letters in pwd:
         if letters.islower():
             has_lower = True
+        if letters.isupper():
+            has_upper = True
+
+    # verifying that the elements were indeed present
     if has_lower is False:
+        return False
+    if has_upper is False:
         return False
     return True
