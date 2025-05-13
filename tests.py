39d61849d4d21b5777fe371b341f2f77305c866c
cedward93 <cedward1993@gmail.com>
2025-05-12 21:55:36 -0700

    added test_short_length to ensure that shorter entries return false (already satisfied). added testing_long_length to ensure passwords above upper bound returns false

--- tests.py
+++ tests.py
@@ -12,6 +12,13 @@ class TestCheckPwd (unittest.TestCase):
         """testing for a valid length"""
         self.assertTrue(check_pwd("Test~length13"))
 
+    def test_short_length(self):
+        """testing for a length less than the min"""
+        self.assertFalse(check_pwd("Test~1"))
+
+    def testing_long_length(self):
+        """testing a length that is more than the length"""
+        self.assertFalse(check_pwd("Test~lengthOverTwenty"))
 
 if __name__ == '__main__':
     unittest.main()
