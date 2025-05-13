aa41809142a27d2fdcc0a33ce5e57e0b61c36ff0
cedward93 <cedward1993@gmail.com>
2025-05-12 22:23:51 -0700

    updated the code to test if a password has a number

--- check_pwd.py
+++ check_pwd.py
@@ -8,6 +8,7 @@ def check_pwd(pwd: str) -> bool:
     # creating variables to test if elements are present in the string
     has_lower = False
     has_upper = False
+    has_digit = False
 
     # itterating through the string to search for elements
     for letters in pwd:
@@ -15,10 +16,14 @@ def check_pwd(pwd: str) -> bool:
             has_lower = True
         if letters.isupper():
             has_upper = True
+        if letters.isdigit():
+            has_digit = True
 
     # verifying that the elements were indeed present
     if has_lower is False:
         return False
     if has_upper is False:
+        return False
+    if has_digit is False:
         return False
     return True
